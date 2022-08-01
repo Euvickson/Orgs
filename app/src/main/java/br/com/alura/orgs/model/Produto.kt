fc5d1a2f3583ef241e5324e4f9998f3af2f2a5e4
Euvickson <victor.eugennio@gmail.com>
2022-08-01 16:21:16 -0300

    Implementando a entidade e o DAO novo dentro do projeto

--- app/src/main/java/br/com/alura/orgs/model/Produto.kt
+++ app/src/main/java/br/com/alura/orgs/model/Produto.kt
@@ -2,13 +2,16 @@ package br.com.alura.orgs.model
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
 
 /*Para usar essa anotação, devemos adicionar um plugin nas dependências do projeto. Ela é útil para criar uma implementação gerada automaticamente, sem haver a necessidade de
 * implementar métodos da classe Parcelable*/
+@Entity
 @Parcelize
 data class Produto(
+        @PrimaryKey(autoGenerate = true) val id: Long = 0L,
         val nome: String,
         val descricao: String,
         val valor: BigDecimal,
