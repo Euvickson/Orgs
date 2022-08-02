33fff5d41517f86d87dd4534a3789af06a19079d
Euvickson <victor.eugennio@gmail.com>
2022-08-02 17:24:13 -0300

    Refatoração de código e utilização do banco de dados no comportamento de salvar o produto e atualizar a lista com o adapter

--- app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -13,7 +13,7 @@ import java.util.*
 
 class ListaProdutosAdapter(
     private val context: Context,
-    produtos: List<Produto>,
+    produtos: List<Produto> = emptyList(),
     var quandoClicado: (produto: Produto) -> Unit = {}
 ) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
 
